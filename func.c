63f4822e656ce6b0839ac99f4a96ed628299be2e
Stanhope Arthur <stanhope.c.arthur@gmail.com>
2023-01-27 20:44:51 +0100

    Second commit

--- func.c
+++ func.c
@@ -141,7 +141,7 @@ int print_int(va_list types, char buffer[],
 	return (write_number(is_negative, i, buffer, flags, width, precision, size));
 }
 
- * print_binary - Prints an unsigned number
+/** print_binary - Prints an unsigned number
  * @types: Lista of arguments
  * @buffer: Buffer array to handle print
  * @flags:  Calculates active flags
